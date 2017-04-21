d27009f569d5d8067d87b005ca0b12f52f08557c
Rupert Saxton <rupertsaxton@gmail.com>
2017-04-21 09:53:56 -0400

    Further class development

--- classes.rb
+++ classes.rb
@@ -1,9 +1,38 @@
 class GamePlay
-  
+  def initialize
+
+  end
+
+  def turn
+  end
+
+  def gameover?
+  end
 end
 
 class Player
+  @@player_icon = ["X", "O"]
+  
+  def initialize
+
+  end
+
 end
 
 class Board
+  def initialize
+    @board = [[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
+  end
+
+  def update(string)
+  end
+
+  def construct
+    puts "_____"
+    @board.each do |subarray|
+      puts subarray.join("|")
+      puts "_____"
+    end
+  end
+
 end
